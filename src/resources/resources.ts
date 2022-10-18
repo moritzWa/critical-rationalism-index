dae19240bb6e2f59e5c35f841cd991b1cd3f6cae
moritzWa <wallawitsch@gmail.com>
2022-10-18 13:06:28 +0200

    add conjecture magazine

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -29,34 +29,40 @@ type PeopleTypes = {
     href: string,
 }
 
-export const People: Record<string, PeopleTypes> = {
+export const People = {
     "David Deutsch": {
         name: 'David Deutsch',
         description: 'David Elieser Deutsch FRS is a British physicist at the University of Oxford. He is a Visiting Professor in the Department of Atomic and Laser Physics at the Centre for Quantum Computation in the Clarendon Laboratory of the University of Oxford.',
         href: 'http://www.daviddeutsch.org.uk/h',
-    },
+    } as PeopleTypes,
     "Brett Hall": {
         name: 'Brett Hall',
         description: "doing ToKCast",
         href: 'https://www.bretthall.org/',
     },
+    "Aaron Stupple": {
+        name: 'Aaron Stupple',
+        description: "Aaron Stupple is the founder of Conjecture Magazine which applies critical rationalism to contemporary issues and the nature of progress. Aaron is an internal medicine physician in Massachusetts.",
+        href: "https://twitter.com/astupple",
+    },
+
 }
 
 // Sources
 
 enum SourceType {
     podcast = "podcast",
+    blog = "blog",
 }
 
 type Source = {
     name: string,
     type: SourceType,
     href: string,
-    authors: [keyof typeof People],
+    authors: (keyof typeof People)[],
 }
 
-// type Keys = keyof typeof employee;
-
+// Note, if you add an Author here pls make sure you add them to the People object above
 export const Sources: Source[] = [
     {
         name: 'ToKCast',
@@ -64,4 +70,16 @@ export const Sources: Source[] = [
         href: 'https://brettroberthall.podbean.com/',
         authors: ['Brett Hall'],
     },
+    {
+        name: 'Conjecture Magazine',
+        type: SourceType.blog,
+        href: 'https://medium.com/conjecture-magazine',
+        authors: ['Aaron Stupple'],
+    },
+    {
+        name: 'The Four Strands',
+        type: SourceType.blog,
+        href: 'https://fourstrands.org/',
+        authors: ['Aaron Stupple'],
+    }
 ]
