c7dd80b462662e70887f735bb448c6da0b7e1483
moritzWa <wallawitsch@gmail.com>
2022-10-18 13:11:31 +0200

    add more ppl

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -39,13 +39,30 @@ export const People = {
         name: 'Brett Hall',
         description: "doing ToKCast",
         href: 'https://www.bretthall.org/',
-    },
+    } as PeopleTypes,
     "Aaron Stupple": {
         name: 'Aaron Stupple',
         description: "Aaron Stupple is the founder of Conjecture Magazine which applies critical rationalism to contemporary issues and the nature of progress. Aaron is an internal medicine physician in Massachusetts.",
         href: "https://twitter.com/astupple",
-    },
-
+    } as PeopleTypes,
+    "Logan Chipkin": { 
+        name: "Logan Chipkin",
+        description: "Logan Chipkin is a writer in Philadelphia. His previous publications can be found on his website. He is also working on a fiction novel, chapters of which can be found here.",
+    }as PeopleTypes,
+    "Dennis Hackethal": { 
+        name: "Dennis Hackethal",
+        description: "Dennis Hackethal is a philosopher and software engineer. His book A Window on Intelligence is aimed at the general reader and covers artificial general intelligence, the evolution of people, AI safety, and more.",
+        href: "https://blog.dennishackethal.com/",
+    } as PeopleTypes,
+    "Ella Hoeppner": { 
+        name: "Ella Hoeppner",
+        description: "Ella Hoeppner is a software engineer with an interest in epistemology, artificial general intelligence, and artificial life. She writes at FourStrands.org and at her website.",
+        href: "https://www.ellahoeppner.com/",
+    } as PeopleTypes,
+    "Bruce Nielson": { 
+        name: "Bruce Nielson",
+        description: "Bruce Nielson runs FourStrands.org and hosts the Theory of Anything podcast. Both explore subjects related to David Deutsch's 'Four Strands'.",
+    } as PeopleTypes,
 }
 
 // Sources
@@ -80,6 +97,13 @@ export const Sources: Source[] = [
         name: 'The Four Strands',
         type: SourceType.blog,
         href: 'https://fourstrands.org/',
-        authors: ['Aaron Stupple'],
+        authors: ['Bruce Nielson'],
+    },
+    // https://anchor.fm/four-strands
+    {
+        name: 'Theory of Anything podcast',
+        type: SourceType.podcast,
+        href: 'https://anchor.fm/four-strands',
+        authors: ['Bruce Nielson'],
     }
 ]
