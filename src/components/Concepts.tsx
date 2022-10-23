1cb3623e095d1c66dabebc2a662212576d47920f
moritzWa <wallawitsch@gmail.com>
2022-10-23 13:13:19 +0200

    refactor concepts

--- src/components/Concepts.tsx
+++ src/components/Concepts.tsx
@@ -1,37 +1,73 @@
 import React from "react"
 import Concept from "./Concept"
 
-const epistemologyConcepts = [
+const ConceptSections = [
   {
-    name: "The growth of human knowledge",
+    name: "Epistemology and Science",
+    description: "small text about the conecpt",
+    conceptAreas: [
+      {
+        name: "The growth of human knowledge",
+        description:
+          "How does human knowledge grow?, How do we know what we know?",
+      },
+      {
+        name: "The science of science",
+        description: "How does science work?",
+      },
+    ],
+  },
+  {
+    name: "Taking Children Seriously",
     description:
-      "How does human knowledge growh?, How do we know what we know? ",
+      "Taking Children Seriously is a parenting movement and educational philosophy whose central idea is that it is possible and desirable to raise and educate children without either doing anything to them against their will or making them do anything against their will.",
+    conceptAreas: [
+      {
+        name: "Why behaviorism is wrong for people.",
+      },
+      {
+        name: "How will credential-less children survive?",
+        url: "https://www.takingchildrenseriously.com/how-will-credential-less-children-survive/",
+      },
+      {
+        name: "How do you raise a child to believe in freedom?",
+        url: "https://www.takingchildrenseriously.com/how-do-you-raise-a-child-to-believe-in-freedom/",
+      },
+      {
+        name: "What matters is the presence of a mind, not the size of the person.",
+      },
+    ],
   },
   {
-    name: "The science of science",
-    description: "How does science work?",
+    name: "Politics and Decision Making",
+    description:
+      "Progress is enabled through error correction; the removal of bad, options, leaders and rules. Critical Rationalism is highly aligned with Libertarianism",
+    conceptAreas: [
+      {
+        name: "The problem of political authority",
+      },
+      {
+        name: "The Open Society and Its Enemies",
+        url: "https://en.wikipedia.org/wiki/The_Open_Society_and_Its_Enemies",
+      },
+    ],
+  },
+  {
+    name: "Memes",
+    description:
+      "Memes are ideas that spread from person to person, and from culture to culture.",
+    conceptAreas: [
+      {
+        name: "Rational and Anti-Rational Memes",
+      },
+      {
+        name: "Memes in Static and Dynamic Societies",
+      },
+    ],
   },
 ]
 
-const takingChildrenSeriouslyConcepts = [
-  {
-    name: "How will credential-less children survive?",
-    url: "https://www.takingchildrenseriously.com/how-will-credential-less-children-survive/",
-  },
-  {
-    name: "How do you raise a child to believe in freedom?",
-    url: "https://www.takingchildrenseriously.com/how-do-you-raise-a-child-to-believe-in-freedom/",
-  },
-]
-
-const memesConcepts = [
-  {
-    name: "Rational and Anti-Rational Memes",
-  },
-  {
-    name: "Memes in Static and Dynamic Societies",
-  },
-]
+const politicsConcepts = []
 
 const Concepts = () => {
   return (
@@ -46,21 +82,14 @@ const Concepts = () => {
         </p>
       </div>
       <div>
-        <Concept
-          name="Epistemology"
-          description="small text about the conecpt"
-          conceptAreas={epistemologyConcepts}
-        />
-        <Concept
-          name="Taking Children Seriously"
-          description="Taking Children Seriously is a parenting movement and educational philosophy whose central idea is that it is possible and desirable to raise and educate children without either doing anything to them against their will or making them do anything against their will."
-          conceptAreas={takingChildrenSeriouslyConcepts}
-        />
-        <Concept
-          name="Memes"
-          description="Memes are ideas that spread from person to person, and from culture to culture. ....."
-          conceptAreas={memesConcepts}
-        />
+        {ConceptSections.map(concept => (
+          <Concept
+            key={concept.name}
+            name={concept.name}
+            description={concept.description}
+            conceptAreas={concept.conceptAreas}
+          />
+        ))}
       </div>
     </div>
   )
