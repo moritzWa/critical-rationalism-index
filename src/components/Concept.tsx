03f5cb5815b1cb8648ab1d0acf3d4e6a99a8de29
moritzWa <wallawitsch@gmail.com>
2022-10-23 13:40:58 +0200

    cleanup markup

--- src/components/Concept.tsx
+++ src/components/Concept.tsx
@@ -8,10 +8,6 @@ type props = {
 
 const Concept = props => {
   return (
-    // <>
-    //   <h2>{props.name}</h2>
-    //   <p>{props.description}</p>
-    // </>
     <div className="bg-white">
       <div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:py-20 lg:px-8">
         <div className="lg:grid lg:grid-cols-3 lg:gap-8">
@@ -19,26 +15,28 @@ const Concept = props => {
             <h2 className="text-3xl font-bold tracking-tight text-gray-900">
               {props.name}
             </h2>
-            <p className="mt-4 text-base text-gray-500">
-              {props.description}
-              {/* <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                customer support
-              </a>{" "}
-              team. */}
-            </p>
+            <p className="mt-4 text-base text-gray-500">{props.description}</p>
           </div>
           <div className="mt-12 lg:col-span-2 lg:mt-0">
             <dl className="space-y-12">
-              {props.conceptAreas.map(faq => (
-                <div key={faq.name}>
-                  <dt className="text-lg font-medium leading-6 text-gray-900">
-                    {faq.name}
-                  </dt>
+              {props.conceptAreas.map(concept => (
+                <div key={concept.name}>
+                  {concept.url ? (
+                    <dt>
+                      <a
+                        href={concept.url}
+                        className="text-lg font-medium leading-6 text-gray-900 underline"
+                      >
+                        {concept.name}
+                      </a>
+                    </dt>
+                  ) : (
+                    <dt className="text-lg font-medium leading-6 text-gray-900">
+                      {concept.name}
+                    </dt>
+                  )}
                   <dd className="mt-2 text-base text-gray-500">
-                    {faq.description}
+                    {concept.description}
                   </dd>
                 </div>
               ))}
