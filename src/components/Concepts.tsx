58cc9c74b16aed2a6576edaa1cb9dcfc85d23fea
moritzWa <wallawitsch@gmail.com>
2022-10-23 19:40:21 +0200

    polish

--- src/components/Concepts.tsx
+++ src/components/Concepts.tsx
@@ -9,7 +9,7 @@ const Concepts = () => {
         <h2 className="text-5xl font-bold tracking-tight text-gray-900">
           Concepts
         </h2>
-        <p id="concepts" className="mt-6 text-xl">
+        <p id="concepts" className="max-w-3xl mt-6 text-xl text-center">
           As a fundamental theory of knowledge, Critical Rationalism spans areas
           such as epistemology, science, politics, decision making, and more.
         </p>
