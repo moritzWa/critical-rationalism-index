0a3ca970436185e8af73b5189b85eedfd0bf976e
moritzWa <wallawitsch@gmail.com>
2022-11-27 18:34:25 -0800

    cleanup

--- src/components/ResourceTable.tsx
+++ src/components/ResourceTable.tsx
@@ -19,7 +19,7 @@ export default function ResourceTable() {
                 <tr>
                   <th
                     scope="col"
-                    className="sticky max-w-[200px] top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                   >
                     Name
                   </th>
