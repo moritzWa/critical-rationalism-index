eb06c4e1243e3b17160a08818d852646e7210a39
moritzWa <wallawitsch@gmail.com>
2022-11-27 17:57:05 -0800

    make newsletter responsive

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,11 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+const useWindowWidth = () => {
+  const isBrowser = typeof window !== "undefined"
+  const [width, setWidth] = useState(isBrowser ? window.innerWidth : 0)
+
+  useEffect(() => {
+    if (!isBrowser) return false
+
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
+  return width
+}
 
 function Newsletter() {
+  const width = useWindowWidth()
+
+  const newsLetterPartWidth = width > 768 ? width : "780"
+  console.log(width, newsLetterPartWidth)
+
   return (
     <div className="w-full m-auto">
       <iframe
         src="https://criticalrationalism.substack.com/embed"
-        width="780"
+        width={newsLetterPartWidth}
         height="320"
         // style={{"border:1px solid #EEE; background:white;"}}
         frameBorder="0"
