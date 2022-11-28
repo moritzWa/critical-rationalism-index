e1c0df40913ca1ea6ba33f427e9440d246c171b2
moritzWa <wallawitsch@gmail.com>
2022-11-27 18:33:26 -0800

    fix wrapping of resource table

--- src/components/ResourceTable.tsx
+++ src/components/ResourceTable.tsx
@@ -12,14 +12,14 @@ export default function ResourceTable() {
         <div className="inline-block min-w-full py-2 align-middle">
           <div className="shadow-sm ring-1 ring-black ring-opacity-5">
             <table
-              className="min-w-full border-separate"
+              className="w-full border-separate"
               style={{ borderSpacing: 0 }}
             >
               <thead className="bg-gray-50">
                 <tr>
                   <th
                     scope="col"
-                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                    className="sticky max-w-[200px] top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                   >
                     Name
                   </th>
@@ -51,7 +51,7 @@ export default function ResourceTable() {
                         rIndex !== Resources.length - 1
                           ? "border-b border-gray-200"
                           : "",
-                        "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8"
+                        "w-min py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8"
                       )}
                     >
                       {resource.name}
