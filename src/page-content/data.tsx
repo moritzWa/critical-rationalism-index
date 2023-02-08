9633ef97a8b86017f1c6529cb5d9d0f4e6478cda
moritzWa <wallawitsch@gmail.com>
2023-02-07 19:15:31 -0800

    fix

--- src/page-content/data.tsx
+++ src/page-content/data.tsx
@@ -184,7 +184,7 @@ export const People = {
     description:
       "Logan Chipkin is a writer based in Philadelphia. His previous publications can be found on his website. He is also working on a fiction novel, chapters of which can be found on his Substack.",
     websiteURL: "http://www.loganchipkin.com/",
-    twitter: "https://twitter.com/ChipkinLogan",
+    twitterURL: "https://twitter.com/ChipkinLogan",
   } as PeopleTypes,
   "Dennis Hackethal": {
     name: "Dennis Hackethal",
